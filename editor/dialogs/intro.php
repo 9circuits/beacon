b84c3c961a50f31c4f1bd43e52ba500711b9cd2b
Nandeep Mali <nandeepmali@n-machine-2.local>
2008-10-22 02:08:30 +0530

    A few more refinements to the UI. Removed the irritating text based close tab button and replaced it with an image. Added Mibbit IRC widget to the help panel for live help!

--- editor/dialogs/intro.php
+++ editor/dialogs/intro.php
@@ -1,1 +1,4 @@
-hello
+<div id="BeaconWelcome" style="text-align: center;">
+    <img src="img/beacon-logo.jpg" alt="Beacon Logo" />
+    <p>Welcome to Beacon. A Collaborative WYSIWYG (and much more!) Editor.</p>
+</div>
