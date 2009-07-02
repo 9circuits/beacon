a3030ba1c1844457ec17b96463c347732789af99
Satya Komaragiri <satya.komaragiri@gmail.com>
2009-07-03 00:23:48 +0530

    initial DTD for docbook added

--- editor/plugins/docbook/js/docbook.js
+++ editor/plugins/docbook/js/docbook.js
@@ -1,14 +1,128 @@
 /*
- * Docbook Plugin for Beacon
- *
- * Copyright Satya Komaragiri and Beacon Team
- * Licensed under GPLv3
- *
- */
+* Docbook Plugin for Beacon
+*
+* Copyright Satya Komaragiri and Beacon Team
+* Licensed under GPLv3
+*
+*/
 
 function docbook_dtd() {
-    var dtd = {};
 
-    return dtd;
+   var dtd = {
+       docbookPara: {
+           type: "block",
+           inlineChildren: ["docbookSGMLTag", "docbookFileName",
+                            "docbookCommand", "docbookOption",
+                            "docbookUserInput", "docbookComputerOutput"],
+           blockChildren: false,
+           siblings: ["docbookScreen", "docbookItemizedList",
+                      "docbookProcedure", "docbookPara"],
+           editorType: "richText",
+           standAlone: true,
+           markup: {
+               tag: "p",
+               attributes: false,
+               sampleText: "This is a sample paragraph."
+           }
+       },
+
+       docbookSection: {
+           type: "block",
+           inlineChildren: false,
+           blockChildren: ["docbookSectionTitle"],
+           siblings: ["docbookSection"],
+           standAlone: true,
+           markup: {
+               requiredChildNodes: ["docbookSectionTitle", "docbookPara"],
+               tag: "div",
+               attributes: {
+                   className: "section"
+               }
+           }
+       },
+
+       docbookSectionTitle: {
+           type: "block",
+           inlineChildren: false,
+           blockChildren: false,
+           standAlone: false,
+           siblings: ["docbookSection"],
+           editorType: "lineedit",
+           markup: {
+               tag: "h2",
+               attributes: false,
+               sampleText: "Sample Section"
+           }
+       },
+
+       docbookSGMLTag: {
+           type: "inline",
+           inlineType: "generic",
+           markup: {
+               tag: "span",
+               attributes: {
+                   className: "sgmltag-element"
+               }
+           }
+       },
+
+       docbookFileName: {
+           type: "inline",
+           inlineType: "generic",
+           markup: {
+               tag: "span",
+               attributes: {
+                   className: "filename"
+               }
+           }
+       },
+
+       docbookCommand: {
+           type: "inline",
+           inlineType: "generic",
+           markup: {
+               tag: "span",
+               attributes: {
+                   className: "command",
+               }
+           }
+       },
+
+       docbookOption: {
+           type: "inline",
+           inlineType: "generic",
+           markup: {
+               tag: "span",
+               attributes: {
+                   className: "option"
+               }
+           }
+       },
+
+       docbookUserInput: {
+           type: "inline",
+           inlineType: "generic",
+           markup: {
+               tag: "span",
+               attributes: {
+                   className: "userinput",
+               }
+           }
+       },
+
+       docbookComputerOutput: {
+           type: "inline",
+           inlineType: "generic",
+           markup: {
+               tag: "span",
+               attributes: {
+                   className: "computeroutput"
+               }
+           }
+       },
+
+
+   };
+
+   return dtd;
 };
-
